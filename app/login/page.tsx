ced40754ed3c7405f378195738f81a4c6b3ddf7c
Bautista Centorbi <bautii.centorbii@gmail.com>
2026-03-13 16:04:06 -0300

    Relative Routes fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginScreen } from "../components/auth/login-screen";
+import { LoginScreen } from "@/components/auth/login-screen";
 
 export default function LoginPage() {
   return <LoginScreen />;
